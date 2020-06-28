92252666c7841f7696f7529b5d7a9979d9a68891
lookout1992 <lookout1992>
2020-06-28 10:27:02 -0400

    new

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ code for sync-fifo, the reading time of SARM is delayed 1 cycle.
 
 # 2020/06/27
 调试环境，目前sync-fifo代码并不能跑通，目前仅上传用于测试。
+
+# 2020/06/28
+重新设计sync_fifo，其中sram读使能有效的下一拍读书据输出。
+本设计sram使用寄存器模型，且使用双口同时读写模型。单口sram设计到读写双ram乒乓操作，不是本设计重点，略过。
--- rtl/sync_fifo.sv
+++ rtl/sync_fifo.sv
@@ -3,8 +3,20 @@
  author : liangwang
  Create Time : 2020-05-24 21:15:05
  */
-
-// `include "sram_32x32_wrap.sv"
+/* ---------设计介绍
+ reg* valid&&ready时序规则：
+ 1.reg0 && reg1都是用switch_en方式，
+   即当sync_fifo_num>0时，reg1_mux2_vld==1，
+     当sync_fifo_num>1时，reg0_mux1_vld==1。
+ sync_fifo 空满判断规则：
+ 1.sram_reg_vld_pre==0,reg0_mux1_vld==0,reg1_mux2_vld==0时，sync_fifo 为空。
+ 2.mux0_sram_rdy==0,reg0_mux1_vld==1,reg1_mux2_vld==1时，sync_fifo 为满。
+ mux*_sel 判断选择规则：
+ 1.mux0_sel默认2;mux1_sel默认1；mux2_sel默认1；
+ 2.reg1_mux2_vld==0，且rd==1时，mux0_sel=2,mux2_sel=1'd1;
+ 3.reg1_mux2_vld==0，且wr==1，rd!=1时，mux0_sel=1,mux1_sel=1,mux2_sel=0;
+ 4.reg1_mux2_vld==1，mux0_sel=0,mux1_sel=0,mux2_sel=0; 
+ */
 
 module Msync_fifo #(parameter DW=32, // data width
 		    parameter FP=32  // fifo depth
@@ -25,204 +37,48 @@ module Msync_fifo #(parameter DW=32, // data width
     output 	    empty, // for read
     output 	    full   // for write    
     );
-
-   // mux 0~2
-   logic [1:0] 	    mux0_sel;
-   logic 	    mux1_sel;
-   logic 	    mux2_sel;  
+   
+   // ==============================================================
+   // define register && wire
+   // ==============================================================
+   // ---------------------model valid&ready&data-------------------
+   // sync_fifo -- input // mux0 -- input
+   logic 	    wr;
+   logic 	    full;
+   logic [DW-1:0]   din;   
+   // sync_fifo -- output // mux2 -- output
+   logic 	    rd;
+   logic 	    empty;
+   logic [DW-1:0]   dout;
+   // mux0 -- output
+   logic 	    mux0_sram_vld,mux0_sram_rdy;
+   logic 	    mux0_mux1_vld,mux0_mux1_rdy;
+   logic 	    mux0_mux2_vld,mux0_mux2_rdy;
    logic [DW-1:0]   mux0_sram;
-   logic [DW-1:0]   mux0_mux1; 
+   logic [DW-1:0]   mux0_mux1;
    logic [DW-1:0]   mux0_mux2;
-   logic [DW-1:0]   sram_reg0;
-   logic [DW-1:0]   reg0_mux1;	       // reg0 output
-   logic [DW-1:0]   mux1_reg1;	       // reg1 input
-   logic [DW-1:0]   reg1_mux2;	       // reg1 output
-   logic 	    mux0_vld,mux0_rdy; // mux1 input
-   logic 	    reg0_vld,reg0_rdy; // reg0 output
-   logic 	    mux1_vld,mux1_rdy; // reg1 input
-   logic 	    reg1_vld,reg1_rdy; // reg1 output
+   // sram -- output
+   logic 	    sram_reg0_vld,sram_reg0_rdy;
+   logic 	    sram_reg0_vld_pre; // 由于sram滞后输出，真实vld为pre寄存器输出，用于sram信号逻辑判断
+   logic 	    sram_reg0_rdy_r;   // 同上，rdy_r为rdy寄存器输出，用于sram信号逻辑判断
+   logic [DW-1:]    sram_reg0;
+   // reg0 -- output
+   logic 	    reg0_mux1_vld,reg0_mux1_rdy;
+   logic [DW-1:0]   reg0_mux1;
+   // mux1 -- output
+   logic 	    mux1_reg1_vld,mux1_reg1_rdy;
+   logic [DW-1:0]   mux1_reg1;
+   // reg1 -- output
+   logic 	    reg1_mux2_vld,reg1_mux2_rdy;
+   logic [DW-1:0]   reg1_mux2;
+   // ---------------------model sel--------------------------------
+   logic [1:0] 	    mux0_sel; // 0:sram;1:mux1;2:mux2
+   logic 	    mux1_sel; // 0:reg0;1:mux0
+   logic 	    mux2_sel; // 0:reg1;1:mux0
    
-   logic 	    reg0_switch;
-   logic 	    sram_empty_r;   
-   logic 	    reg1_switch;
+
    
-   logic 	    wr_sram;
-   logic 	    rd_sram;
-   logic 	    sram_full;
-   logic 	    sram_empty;
-   logic [DW-1:0]   megm[FP-1:0];
-   logic 	    m2m_rd_r;
 
-   logic 	    wr_flag;   
-   logic 	    rd_flag;
-   logic 	    rd_sram_r;   		// delay 1 clock
-   logic [$clog2(FP)-1:0] wr_addr;
-   logic [$clog2(FP)-1:0] rd_addr;
-   
-   // mux0 stage
-   assign mux0_sram = mux0_sel==2'd0 ? din : {DW{1'b0}}; 
-   assign mux0_mux1 = mux0_sel==2'd1 ? din : {DW{1'b0}}; 
-   assign mux0_mux2 = mux0_sel==2'd2 ? din : {DW{1'b0}};
-
-   // mux1 stage
-   assign mux1_reg1 = mux1_sel==1'b0 ? reg0_mux1 : mux0_mux1;
-
-   // mux2 stage
-   assign dout = mux2_sel==1'b0 ? reg1_mux2 : mux0_mux2;
-
-   // register logic
-   // reg0
-   assign reg0_switch = !(reg0_vld&(!reg0_rdy));
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 sram_empty_r <= 'd0;
-      end
-      else begin
-	 sram_empty_r  <= !sram_empty;
-      end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 reg0_mux1 <= 'd0;
-	 reg0_vld  <= 'd0;
-      end
-      else if(reg0_switch)begin
-	 reg0_mux1 <= sram_reg0;
-	 reg0_vld  <= !sram_empty_r;
-      end
-   end
-   assign rd_sram = reg0_switch&(!sram_empty);
-   
-   // reg1
-
-   assign mux1_vld = mux1_sel==1'b1 ? mux0_vld : reg0_vld;   
-   
-   assign reg1_switch = !(reg1_vld&(!reg1_rdy));
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 reg1_mux2 <= 'd0;
-	 reg1_vld  <= 'd0;
-      end
-      else if(reg1_switch)begin
-	 reg1_mux2 <= mux1_reg1;
-	 reg1_vld  <= mux1_vld;
-      end
-   end
-   assign mux1_rdy = reg1_switch;
-
-   // valid && ready
-   // mux0
-   assign full = mux0_sel==2'd0 ? sram_full :
-		 (mux0_sel==2'd1)&(mux1_sel==1'b1) ? !mux1_rdy :
-		 mux0_sel==2'd2 ? !rd : 1'b1;
-   // reg0
-   assign reg0_rdy = (!mux1_sel)&mux1_rdy;
-   // reg1
-   assign reg1_rdy = (!mux2_sel)&rd;
-   // mux2
-   assign empty = !((!sram_empty)|reg0_vld|reg1_vld|wr);
-   
-   // ---------------------------------------------------------
-   // sram
-   // ---------------------------------------------------------
-   // sram
-   assign wr_sram = (mux0_sel==2'd0)&wr;
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 wr_addr <= 'd0;
-	 wr_flag <= 'd0;	 
-      end
-      else if(wr_sram&(wr_addr==FP-1))begin
-	 wr_addr <= 'd0;
-	 wr_flag <= ~wr_flag;
-      end
-      else if(wr_sram&(!sram_full))begin
-	 wr_addr <= wr_addr + 'd1;
-      end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 rd_addr <= 'd0;
-	 rd_flag <= 'd1;	 
-      end
-      else if(rd_sram&(rd_addr==FP-1))begin
-	 rd_addr <= 'd0;
-	 rd_flag <= ~rd_flag;
-      end
-      else if(rd_sram&(!sram_empty))begin
-	 rd_addr <= rd_addr + 'd1;
-      end
-   end
-
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 rd_sram_r <= 'd0;	 
-      end
-      else begin
-	 rd_sram_r <= rd_sram;
-      end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 sram_full <= 'd0;
-      end
-      else if(rd_sram_r&sram_full)begin
-	 sram_full <= 'd0;
-      end
-      else if(wr_sram&(wr_addr==rd_addr-'d1)&(!rd_sram_r)&(wr_flag^~rd_flag))begin
-	 sram_full <= 'd1;
-      end
-   end   
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 sram_empty <= 'd0;
-      end
-      else if(wr_sram&sram_empty)begin
-	 sram_empty <= 'd0;
-      end
-      else if(rd_sram&(rd_addr==wr_addr-'d1)&(!wr_sram)&(wr_flag^rd_flag))begin
-	 sram_empty <= 'd1;
-      end
-   end
-
-   // ---------------------------------------------------------
-   // virtual sram
-   // ---------------------------------------------------------
-   logic [DW-1:0] mem[FP-1:0];
-   initial begin
-      @(!rst_n)
-	for(int i=0; i<FP; i=i+1)begin
-	   mem[i] = 'd0;
-	end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 m2m_rd_r <= 'd0;	 
-      end
-      else begin
-	 m2m_rd_r <= rd_sram;
-      end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(wr_sram)begin
-	 mem[wr_addr] <= mux0_sram;
-      end
-   end
-   always @ (posedge clk or negedge rst_n)begin
-      if(!rst_n)begin
-	 sram_reg0 <= 'd0;	 
-      end
-      else if(m2m_rd_r)begin
-	 sram_reg0 <= mem[rd_addr];
-      end
-   end   
-   
-   // // ---------------------------------------------------------
-   // // logic control
-   // // ---------------------------------------------------------
-   // // mux0
-   // assign mux0_sel = 
-   
    
    
    
